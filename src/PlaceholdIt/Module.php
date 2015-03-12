31d7e1652928eeb6dc0f6073ea75fc3b316b2307
William Urbano <williamurbano93@gmail.com>
2015-03-12 18:19:13 -0300

    Updated Module.php

--- src/PlaceholdIt/Module.php
+++ src/PlaceholdIt/Module.php
@@ -8,10 +8,6 @@ use Zend\ModuleManager\Feature\AutoloaderProviderInterface;
 
 class Module implements ViewHelperProviderInterface, AutoloaderProviderInterface {
 
-	public function onBootstrap() {
-		echo 1; exit;
-	}
-
 	public function getAutoloaderConfig() {
 		return array(
 			'Zend\Loader\StandardAutoloader' => array(
